d948e941760900c90f8b19b8c76d8b188faaaeb5
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-02-05 12:11:46 +0100

    chore(Td-agence):
    implement home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
+import Home from "./logoAgence.svg";
+import Favorites from "./heart.svg";
+import Menu from "./menu.svg";
+import Twitter from "./twitter.svg";
+import Fb from "./facebook.svg";
+import Pinterest from "./pinterest.svg";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={{ width: "100%", height: "100%" }}>
+      <header>
+        <nav>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              height: "130px",
+            }}
+          >
+            <Link to="/home">
+              {" "}
+              <img src={Home} alt="home" className="home" />
+            </Link>
+            <div className="choices">
+              <Link to="/achats" className="liens">
+                Achats
+              </Link>{" "}
+              <Link to="/ventes" className="liens">
+                Ventes
+              </Link>{" "}
+              <Link to="/locations" className="liens">
+                Locations
+              </Link>{" "}
+              <Link to="/favorites" className="liens">
+                {" "}
+                <img src={Favorites} alt="favorites" />
+              </Link>
+              <Link to="/menu">
+                {" "}
+                <img src={Menu} alt="menu" />
+              </Link>
+            </div>
+          </div>
+        </nav>
       </header>
+      <main>
+        <Link to="/exclu" className="exclu">
+          <div className="exclu-content"></div>
+          <div className="descriptionExclu">
+            <h2>House Holiday</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </Link>
+      </main>
+      <footer
+        style={{
+          height: "50px",
+        }}
+      >
+        <div className="infos">
+          <div className="infos-services">
+            <Link to="/history" className="liens">
+              Notre histoire
+            </Link>{" "}
+            <Link to="/services" className="liens">
+              Services clients
+            </Link>{" "}
+            <Link to="/agences" className="liens">
+              Nos agences
+            </Link>{" "}
+          </div>
+          <div className="contacts">
+            <Link to="/twitter" className="liens">
+              {" "}
+              <img src={Twitter} alt="twitter" />
+            </Link>
+            <Link to="/facebook" className="liens">
+              {" "}
+              <img src={Fb} alt="facebook" />
+            </Link>
+            <Link to="/pinterest" className="liens">
+              {" "}
+              <img src={Pinterest} alt="pinterest" />
+            </Link>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
-
-export default App;
