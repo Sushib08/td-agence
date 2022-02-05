7086e4ccecd54d80b416ec6080a6d76c88d1381c
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-02-05 21:37:28 +0100

    feat(td-agence):
    add filter

--- src/header/achats.jsx
+++ src/header/achats.jsx
@@ -5,7 +5,7 @@ import Twitter from "../twitter.svg";
 import Fb from "../facebook.svg";
 import Pinterest from "../pinterest.svg";
 import Glass from "./glass.svg";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import "./achats.css";
 import { getList } from "./list";
 import Lodging1 from "./lodging1.svg";
@@ -14,6 +14,7 @@ import Envelope from "./envelope.svg";
 
 export default function Achats() {
   let achats = getList();
+  let [searchParams, setSearchParams] = useSearchParams();
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <header>
@@ -55,7 +56,19 @@ export default function Achats() {
         <div className="achat-content">
           <h1 style={{ color: "#C2AD74", fontSize: "35px" }}>Achats</h1>
           <div className="search">
-            <input maxLength="14" placeholder="Rechercher..." />
+            <input
+              value={searchParams.get("filter") || ""}
+              onChange={(event) => {
+                let filter = event.target.value;
+                if (filter) {
+                  setSearchParams({ filter });
+                } else {
+                  setSearchParams({});
+                }
+              }}
+              maxLength="14"
+              placeholder="Rechercher..."
+            />
             <button className="glass">
               <img src={Glass} className="glass" alt="glass" />
             </button>
@@ -63,7 +76,13 @@ export default function Achats() {
         </div>
         <div>
           <div className="lodging-content">
-            {achats.map((achat) => (
+          {achats
+          .filter(achat => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = achat.title.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          }).map((achat) => (
               <div className="choice-lodging">
                 <Link to={`/achats/${achat.title}`} key={achat.title}>
                   <img src={Lodging1} alt="lodging" />
@@ -74,14 +93,12 @@ export default function Achats() {
                 </div>
                 <div className="info3">{achat.localisation}</div>
                 <div className="buttons">
-                <Link to="/pinterest" className="button">
-              {" "}
-              <img src={LittleHeart} alt="littleheart" />
-            </Link>
-            <Link to="/Envelope" className="button">
-              {" "}
-              <img src={Envelope} alt="envelope" />
-            </Link>
+                  <button className="button">
+                    <img src={LittleHeart} alt="littleheart" />
+                  </button>
+                  <button className="button">
+                    <img src={Envelope} alt="envelope" />
+                  </button>
                 </div>
               </div>
             ))}
